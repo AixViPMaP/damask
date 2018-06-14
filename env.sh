7fd1b5ca91791ab1f2f9be040ef6eddd4bb1cb32
Lukas Koschmieder <31916683+lukas-koschmieder@users.noreply.github.com>
2018-06-14 16:00:58 +0200

    Customize environment

--- env.sh
+++ env.sh
@@ -1,1 +1,9 @@
-source $DAMASK_DIR/DAMASK_env.sh
+export DAMASK_ROOT=/opt/damask
+export PATH=/opt/damask/bin:/opt/petsc/bin:$PATH
+export PYTHONPATH=/opt/damask/lib:$PYTHONPATH
+
+ulimit -s unlimited 2>/dev/null  # Maximum stack size
+ulimit -d unlimited 2>/dev/null  # Maximum heap size
+ulimit -v unlimited 2>/dev/null  # Maximum virtual memory size
+ulimit -m unlimited 2>/dev/null  # Maximum physical memory size
+ulimit -c 0         2>/dev/null  # Maximum core file size
